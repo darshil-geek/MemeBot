db1afbba4c6e54fb1b42b16ac47cb94498439656
Darshil Shah <58663553+darshil-geek@users.noreply.github.com>
2021-07-10 12:46:30 +0530

    defined sep funcs for image memes and text content

--- MemeBot/bot.py
+++ MemeBot/bot.py
@@ -19,18 +19,15 @@ reddit=praw.Reddit(client_id='', #enter ur reddit app id
                     user_agent="trial"
 )
 
-subreddit= reddit.subreddit("desimemes")
-
 @client.event
 async def on_ready():
     print("ready")
 
-@client.command()
-async def sr(ctx):
-    meme_choices=["Semenretention"]
-    x=random.choice(meme_choices)
+
+
+async  def text_only(subr_choices,ctx):
+    x=random.choice(subr_choices)
     subreddit= await reddit.subreddit(x)
-    #top=subreddit.hot(limit=50)
     all_subs=[]
 
     
@@ -40,7 +37,7 @@ async def sr(ctx):
     name=random_sub.title
     
     text=''.join(random_sub.selftext)
-    
+    text=text[:4096]
     embed = discord.Embed(title=name, url=random_sub.url,
                             description=text,
                            color=discord.Color.random(),
@@ -48,45 +45,38 @@ async def sr(ctx):
     await ctx.send(embed=embed)
 
 
-@client.command()
-async def bnews(ctx):
-    meme_choices=["investing"]
-    x=random.choice(meme_choices)
-    subreddit= reddit.subreddit(x)
-    top=subreddit.hot(limit=50)
+async def img_txt(subr_choices,ctx):
+    x=random.choice(subr_choices)
+    subreddit= await reddit.subreddit(x)
     all_subs=[]
 
-    for submission in top:
-        all_subs.append(submission)
-    random_sub=random.choice(all_subs)
-    name=random_sub.title
-    text=''.join(random_sub.selftext)
     
-    embed = discord.Embed(title=name, url=random_sub.url,
-                            description=text,
-                           color=discord.Color.random(),
-                           )
-    await ctx.send(embed=embed)
-
-@client.command()
-async def progmemes(ctx):
-    meme_choices=["ProgrammerHumor","programmingmemes"]
-    x=random.choice(meme_choices)
-    subreddit= reddit.subreddit(x)
-    top=subreddit.hot(limit=50)
-    all_subs=[]
-
-    for submission in top:
+    async for submission in subreddit.hot(limit=50):
         all_subs.append(submission)
     random_sub=random.choice(all_subs)
     name=random_sub.title
     
     embed = discord.Embed(title=name, url=random_sub.url,
+                            #description=text,
                            color=discord.Color.random(),
                            )
     embed.set_image(url=random_sub.url)
     await ctx.send(embed=embed)
 
+
+@client.command()
+async def r(ctx, type=''):
+    if(type=="sr"):
+        subr_choices=["Semenretention"]  #insert subreddits of your choice here
+        await text_only(subr_choices,ctx)
+
+    elif(type=="bnews"):
+        subr_choices=["investing","personalfinance"]  #insert subreddits of your choice here
+        await text_only(subr_choices,ctx)
+
+    elif(type=="progmemes"):
+        subr_choices=["ProgrammerHumor","programmingmemes"]  #insert subreddits of your choice here
+        await img_txt(subr_choices,ctx)
 
 client.run(disc_token)
 
